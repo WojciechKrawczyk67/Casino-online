5cb8ba9cb4f0d1e427271d790e3b2a9c1b42f643
Szymon Krenz <SzymonKrenz123@gmail.com>
2026-05-26 14:39:45 +0200

    Implementacja generatora liczb od 0 do 36 w klasie Wheel

--- Wheel.h
+++ Wheel.h
@@ -2,25 +2,20 @@
 #ifndef FOLDERNPGWOJCIECHKRAWCZYK_WHEEL_H
 #define FOLDERNPGWOJCIECHKRAWCZYK_WHEEL_H
 
-
 #pragma once
+#include <random>
 
-/**
- * @class Wheel
- * @brief Odpowiada za logikę koła ruletki i losowanie wyników.
- */
 class Wheel {
 public:
-    /**
-     * @brief Konstruktor domyślny.
-     */
     Wheel() {}
 
-    /**
-     * @brief Losuje i zwraca wygrywający numer.
-     * @return Zwycięski numer.
-     */
-    int spin() { return 0; }
+    int spin() { 
+        std::random_device rd; 
+        std::mt19937 gen(rd()); 
+        std::uniform_int_distribution<> distrib(0, 36);
+        
+        return distrib(gen);
+    }
 };
 
-#endif //FOLDERNPGWOJCIECHKRAWCZYK_WHEEL_H
+#endif
